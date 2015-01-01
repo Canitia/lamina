8de1dc1411da63b80d9468c8df80915bcc91deac
Michael Boumann <michaelboumann@gmail.com>
2015-01-01 22:37:57 +0100

    fixed title text not being correct

--- wordpress/wp-content/themes/jade-for-wordpress/header.php
+++ wordpress/wp-content/themes/jade-for-wordpress/header.php
@@ -14,7 +14,7 @@ License URI: http://creativecommons.org/licenses/by-sa/3.0/
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <head>
-<title><?php bloginfo('name'); ?> <?php if ( is_single() ) { ?> » Blog Archive <?php } ?> <?php wp_title(); ?></title>
+<title><?php echo wp_title(); ?></title>
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
 <meta name="generator" content="WordPress <?php bloginfo('version'); ?>" />
