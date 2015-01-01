7f57354516e6f192d53c930d67c9b9c7b188ddd0
Michael Boumann <michaelboumann@gmail.com>
2015-01-01 23:12:35 +0100

    added comments and cleaned up code

--- wordpress/wp-content/themes/jade-for-wordpress/index.php
+++ wordpress/wp-content/themes/jade-for-wordpress/index.php
@@ -1,60 +1,51 @@
-<?php
-/*
-Theme Name: Jade for Wordpress
-Theme URI: http://teamtreehouse.com/wordpress-bootstrap-theme-tutorial
-Description: A theme based on material-design elements. This is the wordpress version of the Ghost original theme.
-Author: Michael Boumann
-Author URI: https://github.com/hxkclan/jade-for-wordpress
-Version: 1.0
-Tags: responsive, white, bootstrap, material design
-
-License: Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
-License URI: http://creativecommons.org/licenses/by-sa/3.0/
-*/
-?>
 <?php get_header(); ?>
 
    <div class="container-fluid">
        <div class="container-inside">
-       <div>
-            <?php if ( have_posts() ) : ?>
+                    <?php if ( have_posts() ) : ?>
 
-                <?php while ( have_posts() ) : the_post(); ?>
-                    <div class="row">
-                        <div>
-                    <div <?php post_class(); ?>>
-                        <h4 class="text-left-title modal-post"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a><p class="text-center postdate"> <p class="text-right postdate"><?php echo 'Written by '; the_author(); ?></p></h4>
+                        <?php while ( have_posts() ) : the_post(); ?>
+                                    <div class="row">
+                                            <div><!-- .post-->
+                                                        <div <?php post_class(); ?>><!--.post-header-->
+                                                                <h4 class="text-left-title modal-post">
+                                                                        <a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
+                                                                        <p class="text-right postdate"><?php echo 'Written by '; the_author(); ?></p></h4>
+                                                        </div><!--.post-header-->
 
-                    </div><!--.post-header-->
+                                                            <div class="entry clear"><!--. entry-->
 
-                        <div class="entry clear">
+                                                                <?php if ( function_exists( 'add_theme_support' ) ) the_post_thumbnail(); ?>
 
-                            <?php if ( function_exists( 'add_theme_support' ) ) the_post_thumbnail(); ?>
+                                                                <?php the_excerpt(); ?>
 
-                            <?php the_excerpt(); ?>
+                                                            </div><!--. entry-->
+                                                                <a href="<?php echo get_permalink(); ?>" class="readmore">Read More</a>
 
-                        </div><!--. entry-->
-                            <a href="<?php echo get_permalink(); ?>" class="readmore">Read More</a>
+                                                        <!-- navigation?-->
+                                                        <div class="navigation">
+                                                            <?php posts_nav_link(); ?>
+                                                        </div>
+                                                        <!-- navigation?-->
 
-                    <!-- navigation?-->
-                    <div class="navigation"><?php posts_nav_link(); ?></div>
+                                                        <!--.post-footer-->
+                                                        <footer class="postfooter">
+                                                                <?php edit_post_link(); ?>
+                                                        </footer>
+                                                            <!--.post-footer-->
 
-                <footer class="postfooter">
-                <?php edit_post_link(); ?>
-                </footer>
-                        <!--.post-footer-->
-        <hr>
-                    </div><!-- .post-->
-                        </div>
-                            <!-- column 1 end! -->
+                                                        <hr>
+                                            </div><!-- .post-->
+                                    </div>
+                                    <!-- column end! -->
 
+                            <!-- error handling -->
+                            <?php endwhile; else: ?>
+                        		      <p><?php _e('Sorry, this page does not exist.'); ?></p>
+                            <?php endif; ?>
 
-        	<?php endwhile; else: ?>
-        		<p><?php _e('Sorry, this page does not exist.'); ?></p>
-        	<?php endif; ?>
+    </div><!-- end container inside -->
+</div><!-- container fluid END! -->
 
-        </div>
-</div>
-    </div><!-- container fluid END! -->
-
+<!-- start of footer -->
 <?php get_footer(); ?>
