e6e465f83dd27903fdda2118782bd411d50f1b03
Michael Boumann <michaelboumann@gmail.com>
2015-01-02 01:00:27 +0100

    add thumbnail to single

--- wordpress/wp-content/themes/jade-for-wordpress/single.php
+++ wordpress/wp-content/themes/jade-for-wordpress/single.php
@@ -9,7 +9,13 @@
             <div class="row">
                 <div>
             <div <?php post_class(); ?>>
-                        <h4 class="text-left-title modal-post"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a><p class="text-right postdate"><?php echo 'Written by '; the_author(); ?></p></h4>
+                        <h4 class="text-left-title modal-post">
+                            <a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
+                            <p class="text-right postdate"><?php echo 'Written by '; the_author(); ?></p>
+                            <?php if ( has_post_thumbnail() ) {
+                            	the_post_thumbnail();
+                            }?>
+                            </h4>
 
             </div><!--.post-header-->
 
