956b71ece436c58aee6475261920badec484db06
Michael Boumann <michaelboumann@gmail.com>
2015-01-02 13:23:36 +0100

    actually fix title this time

--- wordpress/wp-content/themes/jade-for-wordpress/header.php
+++ wordpress/wp-content/themes/jade-for-wordpress/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <head>
-<title><?php wp_title(''); ?></title>
+<title><?php wp_title( '|', true, 'right' ); ?></title>
 
     <!-- some meta -->
     <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
