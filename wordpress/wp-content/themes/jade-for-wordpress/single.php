be07a5c153a9c95edc530ed448a37a6f83a26ff4
Michael Boumann <michaelboumann@gmail.com>
2015-01-03 01:42:55 +0100

    major cleanup removed old code

--- wordpress/wp-content/themes/jade-for-wordpress/single.php
+++ wordpress/wp-content/themes/jade-for-wordpress/single.php
@@ -53,7 +53,7 @@
                             </div><!-- einde md8 -->
 
 
-                        <div class="col-md-4 visible-md">
+                        <div class="col-md-4 visible-md visible-lg">
                             <?php get_sidebar( 'primary' ); ?>
                         </div>
 
@@ -61,60 +61,4 @@
 </div><!-- container fluid END! -->
 
 <!-- start of footer -->
-<?php get_footer(); ?>
-
-
-
-
-
-
-
-
-    <div class="container-fluid">
-            <div class="container-inside">
-            <?php if ( have_posts() ) : ?>
-
-                <?php while ( have_posts() ) : the_post(); ?>
-                    <article>
-                    <div <?php post_class(); ?>>
-                        <div class="modal-post"><!-- post header -->
-                            <h4 class="text-left-title"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h4>
-                                <?php
-                                if ( has_post_thumbnail() ) {
-                                	the_post_thumbnail();
-                                }?>
-
-                            <p class="text-right postdate"><?php echo 'Written by '; the_author(); ?></p>
-                            <p class="text-right postdate"><?php the_tags(); ?></p>
-                        </div>
-
-                        <div class="entry clear">
-
-                            <?php if ( function_exists( 'add_theme_support' ) ) the_post_thumbnail(); ?>
-
-                            <?php the_content(); ?>
-
-                        </div><!--. entry-->
-
-                            <!-- navigation?-->
-                            <?php wp_link_pages(); ?>
-
-                        <!--.post-footer-->
-                        <footer class="postfooter">
-                        <?php edit_post_link(); ?>
-                        </footer>
-                </div><!--.post-header-->
-                </article> <!-- column 1 end! -->
-
-
-        	<?php endwhile; else: ?>
-        		<p><?php _e('Sorry, this page does not exist.'); ?></p>
-        	<?php endif; ?>
-
-        	  	<hr>
-
-
-        </div><!-- container inside END! -->
-</div><!-- container fluid END! -->
-
 <?php get_footer(); ?>
