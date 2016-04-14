ff26ef181de62588e9c2fd9fd1d3fb5082ef18f2
Michael Boumann <mboumann@outlook.com>
2016-04-14 20:30:32 +0200

    new sidebar styling

--- wordpress/wp-content/themes/jade-for-wordpress/sidebar.php
+++ wordpress/wp-content/themes/jade-for-wordpress/sidebar.php
@@ -1,5 +1,4 @@
 	<div class="card card-sidebar"><!-- start of a sidebar item -->
-  <h1 class="text-left-title-featured accentcolor2 center-align">Sidebar</h1>
   <article>
 		<ul>
 			<?php if ( ! dynamic_sidebar('primary') ) : ?>
