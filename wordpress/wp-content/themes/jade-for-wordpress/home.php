3f8881a249c716b7c153a027dc8c54c57b7237c1
Michael Boumann <mboumann@outlook.com>
2016-04-15 01:00:34 +0200

    featured/sticky post slider

--- wordpress/wp-content/themes/jade-for-wordpress/home.php
+++ wordpress/wp-content/themes/jade-for-wordpress/home.php
@@ -1,6 +1,48 @@
 <?php get_header(); ?>
 
 <div class="container-fluid">
+
+  <div class="slider hide-on-small-only">
+                <ul class="slides">
+       <?php
+       $args = array(
+       	'posts_per_page'      => 3,
+       	'post__in'            => get_option( 'sticky_posts' ),
+       	'ignore_sticky_posts' => 1,
+       );
+
+       // The Query
+       $the_query = new WP_Query( $args );
+
+       // The Loop
+       if ( $the_query->have_posts() ) {
+       	while ( $the_query->have_posts() ) {
+       		$the_query->the_post();
+          echo '<li>';
+          if ( has_post_thumbnail() ) {
+            the_post_thumbnail();
+          }
+          echo '<div class="caption center-align"><h3 class="text-left-title-featured accentcolor2">';
+          the_title();
+          echo '</h3></div></li>';
+
+       	}
+       } else {
+       	// no posts found
+       }
+       /* Restore original Post Data */
+       wp_reset_postdata();
+?>
+
+
+
+
+              </ul>
+  </div><!-- .slider -->
+
+
+
+
   <div class="row">
                     <?php if ( have_posts() ) : ?>
                           <div class="main-content col s12 m12 l8">
