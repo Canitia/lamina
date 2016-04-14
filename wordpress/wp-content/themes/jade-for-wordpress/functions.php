b85ecd639737705cb22f8e0d91dd5fbd1521af11
Michael Boumann <mboumann@outlook.com>
2016-04-14 20:30:47 +0200

    add css and classes for styling

--- wordpress/wp-content/themes/jade-for-wordpress/functions.php
+++ wordpress/wp-content/themes/jade-for-wordpress/functions.php
@@ -16,8 +16,8 @@ function my_register_sidebars() {
 			'description' => __( 'A short description of the sidebar.' ),
 			'before_widget' => '<div id="%1$s" class="widget %2$s">',
 			'after_widget' => '</div>',
-			'before_title' => '<li class="collection-header center"><h3 class="widget-title">',
-			'after_title' => '</li></h3>'
+			'before_title' => '<li class="collection-header center"><h1 class="widget-title text-left-title-featured accentcolor2 center-align">',
+			'after_title' => '</li></h1>'
 		)
 	);
 	/* Repeat register_sidebar() code for additional sidebars. */
