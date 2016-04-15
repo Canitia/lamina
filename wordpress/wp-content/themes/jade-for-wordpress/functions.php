91317b7c6f9108680e4aeff2c5db7b5acdf3a427
Michael Boumann <mboumann@outlook.com>
2016-04-15 23:40:51 +0200

    switch to png

--- wordpress/wp-content/themes/jade-for-wordpress/functions.php
+++ wordpress/wp-content/themes/jade-for-wordpress/functions.php
@@ -9,7 +9,7 @@ $args = array(
 	'width'         => 980,
 	'flex-height'    => true,
 	'height'        => 200,
-	'default-image' => get_template_directory_uri() . '/images/header.jpg',
+	'default-image' => get_template_directory_uri() . '/images/header.png',
 );
 add_theme_support( 'custom-header', $args );
 
