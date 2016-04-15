1d2ed1557dde9232d6d7a6e2a0a24e11d8fa365b
Michael Boumann <mboumann@outlook.com>
2016-04-15 11:38:44 +0200

    pagination added

--- wordpress/wp-content/themes/jade-for-wordpress/home.php
+++ wordpress/wp-content/themes/jade-for-wordpress/home.php
@@ -74,9 +74,22 @@
                             <?php endif; ?>
 
                             <!-- navigation?-->
-                            <div class="pagination">
-                                <?php posts_nav_link(); ?>
-                            </div>
+
+                            <ul class="pagination accentcolor2 center-align" role="pagination">
+                              <?php if( get_previous_posts_link() ) :
+
+                              previous_posts_link( '<li class="pagination-arrows newer-posts"><i class="fa fa-arrow-left fa-2x"></i></li>' );
+
+                              endif; ?>
+
+                              <li class="active"><?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; echo $paged; ?></li>
+                              <?php if( get_next_posts_link() ) :
+
+                              next_posts_link( '<li class="pagination-arrows older-posts"><i class="fa fa-arrow-right fa-2x"></i></li>' );
+
+                              endif; ?>
+                            </ul>
+
                             <!-- navigation?-->
 
                     </div><!-- einde md8 -->  <!-- column end! -->
