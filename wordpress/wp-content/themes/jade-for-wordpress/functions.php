a9839216ef2037d9f13e3a4c3261891a5909d60a
Michael Boumann <mboumann@outlook.com>
2016-04-15 23:16:31 +0200

    remove some old code

--- wordpress/wp-content/themes/jade-for-wordpress/functions.php
+++ wordpress/wp-content/themes/jade-for-wordpress/functions.php
@@ -1,6 +1,4 @@
-<?php if ( ! isset( $content_width ) ) {
-	$content_width = 600;
-}
+<?php
 add_theme_support( "title-tag" );
 add_theme_support( 'automatic-feed-links' );
 add_theme_support( "post-thumbnails" );
