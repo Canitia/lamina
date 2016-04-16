6c67dbd2707b2b8d65ddef974e7adce95bcb7f18
Michael Boumann <mboumann@outlook.com>
2016-04-16 02:32:00 +0200

    change index

--- index.php
+++ index.php
@@ -1,49 +1,77 @@
 <?php get_header(); ?>
 
-     <div class="row">
+  <div class="slider hide-on-small-only">
+                <ul class="slides">
+       <?php
+       $args = array(
+       	'posts_per_page'      => 3,
+       	'post__in'            => get_option( 'sticky_posts' ),
+       	'ignore_sticky_posts' => 1,
+       );
+
+       // The Query
+       $the_query = new WP_Query( $args );
+
+       // The Loop
+       if ( $the_query->have_posts() ) {
+       	while ( $the_query->have_posts() ) {
+       		$the_query->the_post();
+          echo '<li>';
+           if ( has_post_thumbnail() ) : ?>
+              <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                  <?php the_post_thumbnail('large'); ?>
+              </a>
+          <?php endif;
+          echo '<div class="caption center-align"><h3 class="text-left-title-featured accentcolor2">';
+          the_title();
+          echo '</h3></div></li>';
+
+       	}
+       } else {
+       	// no posts found
+       }
+       /* Restore original Post Data */
+       wp_reset_postdata();
+?>
+
+
+
+
+              </ul>
+  </div><!-- .slider -->
+
+
+
+
+  <div class="row">
                     <?php if ( have_posts() ) : ?>
                           <div class="main-content col s12 m12 l8">
                         <?php while ( have_posts() ) : the_post(); ?>
 
-                                    <div <?php post_class(); ?>>
-
-                                    <article>
-                                                    <h4 class="text-left-title"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h4>
-                                                        <?php
-                                                        if ( has_post_thumbnail() ) {
-                                                        	the_post_thumbnail('large');
-                                                        }?>
-
-                                                    <p class="text-right postdate"><?php echo 'Written by '; the_author(); ?></p>
-                                                    <p class="text-right postdate"><?php the_tags(); ?></p>
-
-
-                                                <div class="entry clear"><!--. entry-->
-
-                                                    <?php if ( function_exists( 'add_theme_support' ) ) the_post_thumbnail(); ?>
-
-                                                    <?php the_excerpt(); ?>
-
-                                                </div><!--. entry-->
-
-                                            <!--.post-footer-->
-                                            <footer>
-                                                    <a href="<?php echo get_permalink(); ?>" class="readmore">Read More</a>
-
-                                                    <?php edit_post_link(); ?>
-                                            </footer>
-                                                <!--.post-footer-->
-
-                                    </article><!-- close article -->
-
-                                    <hr>
-
-                            </div><!-- close post class div -->
+                                      <div class="card hoverable">
+                                        <article>
+                                              <div>
+                                                <?php if ( has_post_thumbnail() ) : ?>
+                                                    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                                                        <?php the_post_thumbnail(); ?>
+                                                    </a>
+                                                <?php endif; ?>
+                                                  </a>
+                                            </div>
+                                            <div>
+                                                      <h1 class="text-left-title center-align"><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h1>
+                                                      <p class="postdate center-align"><i class="fa fa-clock-o"></i><time> <?php echo get_the_date(); ?></time></p>
+                                                                <?php the_excerpt(); ?>
+                                            </div>
+                                        </article>
+                                        </div>
 
                             <!-- error handling -->
                             <?php endwhile; else: ?>
                         		      <p><?php echo wpautop( 'Sorry, seems there are no posts available' ); ?></p>
                             <?php endif; ?>
+
+                            <!-- navigation?-->
 
                             <ul class="pagination accentcolor2 center-align" role="pagination">
                               <?php if( get_previous_posts_link() ) :
@@ -59,6 +87,7 @@
 
                               endif; ?>
                             </ul>
+
                             <!-- navigation?-->
 
                     </div><!-- einde md8 -->  <!-- column end! -->
