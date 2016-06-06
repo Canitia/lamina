6f426360aae3907aff44bbf606a217a50dee94d4
Michael Boumann <mboumann@outlook.com>
2016-06-07 01:23:57 +0200

    new error page

--- 404.php
+++ 404.php
@@ -3,23 +3,15 @@
    <div class="container-fluid">
      <div class="row">
                     <?php if ( have_posts() ) : ?>
-                          <div class="main-content col s12 m12 l8">
-                            <div class="card card-error big">
-                                  <article>
-                                    <h1 class="text-left-title accentcolor-error center-align">Whoops...</h1>
-                                        <div class="col s12">
-                                          <p>This post has been removed or went to the dark side. To continue please select a option from the navigation menu or go back a page.<p>
-                                      </div>
-                                  </article>
-                                  </div>
-                            <!-- navigation?-->
-
-                    </div><!-- einde md8 -->  <!-- column end! -->
-
-                    <!-- second column (widget bar) -->
-                    <div class="col s12 m12 l4 hide-on-med-and-down">
-                        <?php get_sidebar( 'primary' ); ?>
-                    </div>
+                      <div class="card large card-error">
+                          <div class="card-image center">
+                            <i class="fa fa-ban fa-5x" aria-hidden="true"></i>
+                            <span class="card-title"><p class="posttitle">Error</p></span>
+                          </div>
+                          <div class="card-content">
+                            <p>This page doesn't exist anymore or was available on the old website. To continue please select a menu option from the navigation menu.</p>
+                          </div>
+                      </div>
 
     </div><!-- end container inside -->
 </div><!-- container fluid END! -->
