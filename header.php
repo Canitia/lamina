f1bd0715b093d0b3aea7f75e7194547f087052fc
Michael Boumann <mboumann@outlook.com>
2016-06-07 00:56:03 +0200

    add style.screen.css

--- header.php
+++ header.php
@@ -9,6 +9,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- my own css file (style.css) -->
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/style.screen.css" type="text/css" type="text/css" />
+
     <!-- other used items -->
     <link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
