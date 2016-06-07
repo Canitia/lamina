44100e1cf245eb7bd7cb0628b67e109a81a7c13e
Michael Boumann <mboumann@outlook.com>
2016-06-07 18:55:55 +0200

    remove non-recommended function

--- author.php
+++ author.php
@@ -18,7 +18,7 @@
                                          <?php if ( has_post_thumbnail() ) {
                                                        the_post_thumbnail( 'medium', array( 'class' => 'responsive-img' ) );
                                                } else { ?>
-                                                 <img src="<?php bloginfo('template_directory'); ?>/images/no-pic-available.jpg" alt="<?php the_title_attribute(); ?>" />
+                                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/no-pic-available.jpg" alt="<?php the_title_attribute(); ?>" />
                                                  <?php }; ?>
                                          </a>
                                          <span class="card-title">
