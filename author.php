ec185ec7360630d1f8e0493b3af982cd9c0efd19
Michael Boumann <mboumann@outlook.com>
2016-06-07 03:41:24 +0200

    add pagination to author

--- author.php
+++ author.php
@@ -39,6 +39,24 @@
                              <?php endif; ?>
 
                              <?php wp_link_pages('before=<ul class="pagination accentcolor2 center-align" role="pagination">&after=</ul>&link_before=<li>&link_after=</li>'); ?>
+
+                             <!-- navigation?-->
+
+                             <ul class="pagination accentcolor2 center-align" role="pagination">
+                               <?php if( get_previous_posts_link() ) :
+
+                               previous_posts_link( '<li class="pagination-arrows newer-posts"><i class="fa fa-arrow-left fa-2x"></i></li>' );
+
+                               endif; ?>
+
+                               <li class="active"><?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; echo $paged; ?></li>
+                               <?php if( get_next_posts_link() ) :
+
+                               next_posts_link( '<li class="pagination-arrows older-posts"><i class="fa fa-arrow-right fa-2x"></i></li>' );
+
+                               endif; ?>
+                             </ul>
+
                            </div><!-- einde md8 -->
 
      <div class="col l4 hide-on-med-and-down">
