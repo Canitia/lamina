2eac2a13c2bb34a40ccbe5f3d64f741ca6e0defa
Michael Boumann <mboumann@outlook.com>
2016-06-07 03:05:57 +0200

    close h1 before li tag as it should

--- functions.php
+++ functions.php
@@ -23,7 +23,7 @@ function my_register_sidebars() {
 			'before_widget' => '<div id="%1$s" class="widget %2$s">',
 			'after_widget' => '</div>',
 			'before_title' => '<li class="collection-header center"><h1 class="widget-title text-left-title-featured-sidebar accentcolor2 center-align">',
-			'after_title' => '</li></h1>'
+			'after_title' => '</h1></li>'
 		)
 	);
 	/* Repeat register_sidebar() code for additional sidebars. */
