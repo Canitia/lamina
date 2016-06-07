191eefdd88bd4527d9481bbaa71021e6f0b9b95b
Michael Boumann <mboumann@outlook.com>
2016-06-07 19:09:16 +0200

    force content width

--- functions.php
+++ functions.php
@@ -1,4 +1,8 @@
 <?php
+if ( ! isset( $content_width ) ) {
+	$content_width = 600;
+}
+
 add_theme_support( "title-tag" );
 add_theme_support( 'automatic-feed-links' );
 add_theme_support( "post-thumbnails" );
