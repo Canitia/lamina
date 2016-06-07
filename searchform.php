109d471c58e0ae6a92fb0b1a99e35b8c9173abd5
Michael Boumann <mboumann@outlook.com>
2016-06-07 23:54:12 +0200

    add btn class

--- searchform.php
+++ searchform.php
@@ -7,7 +7,7 @@
               value="<?php echo get_search_query() ?>" name="s"
               title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
       </label>
-      <input type="submit" class="search-submit"
+      <input type="submit" class="search-submit btn"
           value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
   </form>
 </div>
