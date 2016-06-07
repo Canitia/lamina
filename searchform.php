6a4eb641615a22138329f476cdaa386e88a14881
Michael Boumann <mboumann@outlook.com>
2016-06-07 18:16:28 +0200

    fix search form

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <div class="search-box">
 <form method="get" id="searchform" action="<?php echo home_url() ; ?>/">
-<input type="text" value="<?php echo esc_html($s, 1); ?>" name="s" id="s" maxlength="33">  <i class="fa fa-search" aria-hidden="true"></i> />
+<input type="text" value="<?php echo esc_html($s, 1); ?>" name="s" id="s" maxlength="33" />  <i class="fa fa-search" aria-hidden="true"></i>
 </form>
 </div>
