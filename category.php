e493b6ffd74b449b1998389df4fc57b43aa35d43
Michael Boumann <mboumann@outlook.com>
2016-06-08 14:22:25 +0200

    fix category page?

--- category.php
+++ category.php
@@ -1,60 +1,62 @@
 <?php get_header(); ?>
      <div class="row">
+        <div class="main-content col s12 m12 l8">
           <div class="tag_heading center-align">
           <h3><?php single_cat_title(); ?></h3>
           <hr />
           </div>
 
-                                      <?php while ( have_posts() ) : the_post(); ?>
-                                                    <div class="card large">
-                                                      <p class="postdate right"><i class="fa fa-clock-o"></i><time><?php echo get_the_date(); ?></time>
-                                                        <div class="card-image">
-                                                          <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-                                                          <?php if ( has_post_thumbnail() ) {
-                                                                        the_post_thumbnail( 'medium', array( 'class' => 'responsive-img' ) );
-                                                                } else { ?>
-                                                                  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/no-pic-available.jpg" alt="<?php the_title_attribute(); ?>" />
-                                                                  <?php }; ?>
-                                                          </a>
-                                                          <span class="card-title">
-                                                            <p class="posttitle"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></p>
-                                                            </span>
-                                                        </div>
-                                                        <div class="card-content">
-                                                          <?php the_excerpt(); ?>
-                                                        </div>
-                                                      </div>
 
-                                          <!-- error handling -->
-                                          <?php endwhile; else: ?>
-                                                <p><?php echo wpautop( 'Sorry, seems there are no posts available' ); ?></p>
-                                          <?php endif; ?>
+                                  <?php while ( have_posts() ) : the_post(); ?>
+                                                <div class="card large">
+                                                  <p class="postdate right"><i class="fa fa-clock-o"></i><time><?php echo get_the_date(); ?></time>
+                                                    <div class="card-image">
+                                                      <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                                                      <?php if ( has_post_thumbnail() ) {
+                                                                    the_post_thumbnail( 'medium', array( 'class' => 'responsive-img' ) );
+                                                            } else { ?>
+                                                              <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/no-pic-available.jpg" alt="<?php the_title_attribute(); ?>" />
+                                                              <?php }; ?>
+                                                      </a>
+                                                      <span class="card-title">
+                                                        <p class="posttitle"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></p>
+                                                        </span>
+                                                    </div>
+                                                    <div class="card-content">
+                                                      <?php the_excerpt(); ?>
+                                                    </div>
+                                                  </div>
 
-                                          <!-- navigation?-->
+                                      <!-- error handling -->
+                                      <?php endwhile; else: ?>
+                                  		      <p><?php echo wpautop( 'Sorry, seems there are no posts available' ); ?></p>
+                                      <?php endif; ?>
 
-                                          <ul class="pagination accentcolor2 center-align" role="pagination">
-                                            <?php if( get_previous_posts_link() ) :
+                                      <!-- navigation?-->
 
-                                            previous_posts_link( '<li class="pagination-arrows newer-posts"><i class="fa fa-arrow-left fa-2x"></i></li>' );
+                                      <ul class="pagination accentcolor2 center-align" role="pagination">
+                                        <?php if( get_previous_posts_link() ) :
 
-                                            endif; ?>
+                                        previous_posts_link( '<li class="pagination-arrows newer-posts"><i class="fa fa-arrow-left fa-2x"></i></li>' );
 
-                                            <li class="active"><?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; echo $paged; ?></li>
-                                            <?php if( get_next_posts_link() ) :
+                                        endif; ?>
 
-                                            next_posts_link( '<li class="pagination-arrows older-posts"><i class="fa fa-arrow-right fa-2x"></i></li>' );
+                                        <li class="active"><?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; echo $paged; ?></li>
+                                        <?php if( get_next_posts_link() ) :
 
-                                            endif; ?>
-                                          </ul>
+                                        next_posts_link( '<li class="pagination-arrows older-posts"><i class="fa fa-arrow-right fa-2x"></i></li>' );
 
-                                          <!-- navigation?-->
+                                        endif; ?>
+                                      </ul>
 
-                                  </div><!-- einde md8 -->  <!-- column end! -->
+                                      <!-- navigation?-->
 
-                                  <!-- second column (widget bar) -->
-                                  <div class="col s12 m12 l4 hide-on-med-and-down">
-                                      <?php get_sidebar( 'primary' ); ?>
-                                  </div>
+                              </div><!-- einde md8 -->  <!-- column end! -->
+
+                              <!-- second column (widget bar) -->
+                              <div class="col s12 m12 l4 hide-on-med-and-down">
+                                  <?php get_sidebar( 'primary' ); ?>
+                              </div>
 
                   </div><!-- end container inside -->
               </div><!-- container fluid END! -->
