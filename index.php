72271e1c08345ee816676ef0938c6a908b44a9a2
Michael Boumann <mboumann@outlook.com>
2016-06-08 15:11:35 +0200

    move slider outside of main content again

--- index.php
+++ index.php
@@ -2,51 +2,50 @@
 
   <div class="row">
                     <?php if ( have_posts() ) : ?>
+                      <div class="slider hide-on-small-only">
+                          <ul class="slides">
+                           <?php
+                           $args = array(
+                            'posts_per_page'      => 3,
+                            'post__in'            => get_option( 'sticky_posts' ),
+                            'ignore_sticky_posts' => 0,
+                           );
+
+                           // The Query
+                           $the_query = new WP_Query( $args );
+
+                           // The Loop
+                           if ( $the_query->have_posts() ) {
+                            while ( $the_query->have_posts() ) {
+                              $the_query->the_post();
+                              echo '<li>';
+                              echo '<p class="postdate right"><i class="fa fa-star"></i> Featured</p>';?>
+                              <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                              <?php if ( has_post_thumbnail() ) {
+                                            the_post_thumbnail( 'medium', array( 'class' => 'responsive-img' ) );
+                                    } else { ?>
+                                      <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/no-pic-available.jpg" alt="<?php the_title_attribute(); ?>" />
+                                      <?php }; ?>
+                              </a><?php
+                              echo '<div class="caption center-align"><h3 class="text-left-title-featured accentcolor2">';
+                              the_title();
+                              echo '</h3></div></li>';
+
+                            }
+                           } else {
+                            // no posts found
+                           }
+                           /* Restore original Post Data */
+                           wp_reset_postdata();
+                    ?>
+
+
+
+
+                                  </ul>
+                      </div><!-- .slider -->
+
                           <div class="main-content col s12 m12 l8">
-
-                            <div class="slider hide-on-small-only">
-                                <ul class="slides">
-                                 <?php
-                                 $args = array(
-                                 	'posts_per_page'      => 3,
-                                 	'post__in'            => get_option( 'sticky_posts' ),
-                                 	'ignore_sticky_posts' => 0,
-                                 );
-
-                                 // The Query
-                                 $the_query = new WP_Query( $args );
-
-                                 // The Loop
-                                 if ( $the_query->have_posts() ) {
-                                 	while ( $the_query->have_posts() ) {
-                                 		$the_query->the_post();
-                                    echo '<li>';
-                                    echo '<p class="postdate right"><i class="fa fa-star"></i> Featured</p>';?>
-                                    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-                                    <?php if ( has_post_thumbnail() ) {
-                                                  the_post_thumbnail( 'medium', array( 'class' => 'responsive-img' ) );
-                                          } else { ?>
-                                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/no-pic-available.jpg" alt="<?php the_title_attribute(); ?>" />
-                                            <?php }; ?>
-                                    </a><?php
-                                    echo '<div class="caption center-align"><h3 class="text-left-title-featured accentcolor2">';
-                                    the_title();
-                                    echo '</h3></div></li>';
-
-                                 	}
-                                 } else {
-                                 	// no posts found
-                                 }
-                                 /* Restore original Post Data */
-                                 wp_reset_postdata();
-                          ?>
-
-
-
-
-                                        </ul>
-                            </div><!-- .slider -->
-
 
                         <?php while ( have_posts() ) : the_post(); ?>
                                       <div class="card large">
