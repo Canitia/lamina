15b0af2b194061e5976ee14e071103d4c45cc795
Michael Boumann <mboumann@outlook.com>
2016-06-09 16:56:51 +0200

    add new file urls

--- functions.php
+++ functions.php
@@ -12,9 +12,9 @@ function insideuwp_enqueue_script() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'jquery');
-	wp_enqueue_script( 'materialize', trailingslashit( get_template_directory_uri() ) . 'css/materialize.min.css', false );
-	wp_enqueue_script( 'materialize', trailingslashit( get_template_directory_uri() ) . 'js/materialize.min.js', false );
-	wp_enqueue_script( 'font-awesome', trailingslashit( get_template_directory_uri() ) . 'css/font-awesome.min.css', false );
+	wp_enqueue_script( 'materialize', get_stylesheet_directory_uri() . 'css/materialize.min.css', false );
+	wp_enqueue_script( 'materialize', get_stylesheet_directory_uri() . 'js/materialize.min.js', false );
+	wp_enqueue_script( 'font-awesome', get_stylesheet_directory_uri() . 'css/font-awesome.min.css', false );
 }
 add_action( 'wp_enqueue_style', 'insideuwp_enqueue_script' );
 add_action( 'wp_enqueue_scripts', 'insideuwp_enqueue_script' );
