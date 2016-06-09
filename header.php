007c06ba6c8e019cb2b422b958b52fd7c3745a1d
Michael Boumann <mboumann@outlook.com>
2016-06-09 20:08:46 +0200

    fix site icon?

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
     <meta name="generator" content="WordPress <?php bloginfo('version'); ?>" />
     <meta name="description" content="<?php bloginfo( 'description' ); ?>" />
-    <link rel="shortcut icon" type="image/png" href="<?php if ( function_exists( 'the_custom_logo' ) ) {  the_custom_logo(); }?>" />
+    <link rel="icon" type="image/png" href="<?php the_custom_logo(); ?>" />
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
