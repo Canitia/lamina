a999d6666a98c21f603ec5735634dbec122736f8
Michael Boumann <mboumann@outlook.com>
2016-06-09 14:18:07 +0200

    add featured star

--- index.php
+++ index.php
@@ -6,7 +6,12 @@
                         if ( have_posts() ) : while ( have_posts() ) : the_post();
                         ?>
                                       <div class="card large">
-                                        <p class="postdate right"><i class="fa fa-clock-o"></i><time><?php echo get_the_date(); ?></time></p>
+                                        <p class="postdate right">
+                                          <i class="fa fa-clock-o"></i><time><?php echo get_the_date(); ?></time>
+                                          <?php if( is_sticky() ) {
+                                            ?><i class="fa fa-star"></i>
+                                        <?php  } ?>
+                                        </p>
                                           <div class="card-image">
                                             <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
                                             <?php if ( has_post_thumbnail() ) {
