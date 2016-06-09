e556badd7bfee932e1785899b91bc26b74692ff4
Michael Boumann <mboumann@outlook.com>
2016-06-09 11:22:38 +0200

    remove pagination from category page

--- category.php
+++ category.php
@@ -33,23 +33,6 @@
                <?php endif; ?>
 
                <!-- navigation?-->
-
-               <ul class="pagination accentcolor2 center-align" role="pagination">
-                 <?php if( get_previous_posts_link() ) :
-
-                 previous_posts_link( '<li class="pagination-arrows newer-posts"><i class="fa fa-arrow-left fa-2x"></i></li>' );
-
-                 endif; ?>
-
-                 <li class="active"><?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; echo $paged; ?></li>
-                 <?php if( get_next_posts_link() ) :
-
-                 next_posts_link( '<li class="pagination-arrows older-posts"><i class="fa fa-arrow-right fa-2x"></i></li>' );
-
-                 endif; ?>
-               </ul>
-
-               <!-- navigation?-->
      </div>
        <!-- second column (widget bar) -->
        <div class="col s12 m12 l4 hide-on-med-and-down">
