0aa1cee14b5164b1c68631d95ced09ddab77daf9
Michael Boumann <mboumann@outlook.com>
2016-06-09 17:18:31 +0200

    try dependencies feature for queue scripts

--- functions.php
+++ functions.php
@@ -14,10 +14,13 @@ function insideuwp_theme_name_scripts() {
 	wp_enqueue_style( 'materialize', get_stylesheet_directory_uri() . '/css/materialize.min.css', false );
 	wp_enqueue_style( 'font-awesome', get_stylesheet_directory_uri() . '/css/font-awesome.min.css', false );
 	wp_enqueue_script( 'jquery', get_stylesheet_directory_uri() . '/js/jquery-2.2.4.min.js', false );
-	wp_enqueue_script( 'materialize', get_stylesheet_directory_uri() . '/js/materialize.min.js', false );
-	wp_enqueue_script( 'sidenav', get_stylesheet_directory_uri() . '/js/sidenav.js', false );
+	wp_enqueue_script( 'materialize', get_stylesheet_directory_uri() . '/js/materialize.min.js', array('jquery'), false );
+	wp_enqueue_script( 'sidenav', get_stylesheet_directory_uri() . '/js/sidenav.js', array('jquery', 'materialize'), false );
 }
 add_action( 'wp_enqueue_scripts', 'insideuwp_theme_name_scripts' );
+
+
+$handle, $src = false, $deps = array(), $ver = false, $media = 'all'
 
 add_theme_support( "title-tag" );
 add_theme_support( 'automatic-feed-links' );
