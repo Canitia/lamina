8682d9639d133fc759d4f0d46a604e6f6717621f
Michael Boumann <mboumann@outlook.com>
2016-06-09 18:23:41 +0200

    switch to jquery instead of $

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function(){
   // Initialize collapse button
-  $('.button-collapse').sideNav({
+  jQuery('.button-collapse').sideNav({
         edge: 'right', // Choose the horizontal origin
         closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
       }
