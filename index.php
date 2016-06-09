5cc6359cc2a4016c64f468482901a20a7b48a442
Michael Boumann <mboumann@outlook.com>
2016-06-09 14:39:36 +0200

    readd pagination

--- index.php
+++ index.php
@@ -33,6 +33,23 @@
                             <?php endwhile; else: ?>
                         		      <p><?php echo wpautop( 'Sorry, seems there are no posts available' ); ?></p>
                             <?php endif; ?>
+
+                            <!-- navigation?-->
+
+                          <ul class="pagination accentcolor2 center-align" role="pagination">
+                            <?php if( get_previous_posts_link() ) :
+
+                            previous_posts_link( '<li class="pagination-arrows newer-posts"><i class="fa fa-arrow-left fa-2x"></i></li>' );
+
+                            endif; ?>
+
+                            <li class="active"><?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; echo $paged; ?></li>
+                            <?php if( get_next_posts_link() ) :
+
+                            next_posts_link( '<li class="pagination-arrows older-posts"><i class="fa fa-arrow-right fa-2x"></i></li>' );
+
+                            endif; ?>
+                          </ul>
                     </div><!-- einde md8 -->  <!-- column end! -->
 
                     <!-- second column (widget bar) -->
