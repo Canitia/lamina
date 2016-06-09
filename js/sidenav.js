05651b801d6c1327c2767cb362994b06f808213f
Michael Boumann <mboumann@outlook.com>
2016-06-09 18:18:51 +0200

    remove comment

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function(){
-  // Activate the side menu
   // Initialize collapse button
   $('.button-collapse').sideNav({
         edge: 'right', // Choose the horizontal origin
