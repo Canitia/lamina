763976c8f9c0c3bb6ae952fae876fea8471bedd9
Michael Boumann <mboumann@outlook.com>
2016-06-09 11:25:07 +0200

    move maincontent div up

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
   <div class="row">
+    <div class="main-content col s12 m12 l8">
                     <?php
                     $args = array(
                      'posts_per_page' => 3,
@@ -9,7 +10,6 @@
                     $query = new WP_Query( $args );
 
                     if ( $query->have_posts() ) : ?>
-                          <div class="main-content col s12 m12 l8">
                             <div class="slider hide-on-small-only">
                                 <ul class="slides">
                                  <?php
