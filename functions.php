39f6e60717b114415fc91e7ea13ba0f0231d1459
Michael Boumann <mboumann@outlook.com>
2016-06-09 16:35:10 +0200

    add style.screen.css to queue

--- functions.php
+++ functions.php
@@ -76,6 +76,7 @@ function theme_prefix_the_custom_logo() {
 
 function themeslug_enqueue_script() {
 	wp_enqueue_style( 'theme-stylesheet', get_stylesheet_uri(), false );
+	wp_enqueue_style( 'theme-stylesheet-screen', trailingslashit( get_template_directory_uri() ) . 'css/style.screen.css', false );
 	wp_enqueue_script( 'materializecss', trailingslashit( get_template_directory_uri() ) . 'css/materialize.min.css', false );
 	wp_enqueue_script( 'jquery', trailingslashit( get_template_directory_uri() ) . 'js/jquery-2.2.4.min.js', false );
 	wp_enqueue_script( 'materializejs', trailingslashit( get_template_directory_uri() ) . 'js/materialize.min.js', false );
