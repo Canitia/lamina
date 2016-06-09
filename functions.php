8acf2e724ff140ba10b2f3868b28363911bed8cd
Michael Boumann <mboumann@outlook.com>
2016-06-09 16:45:56 +0200

    fix names?

--- functions.php
+++ functions.php
@@ -78,9 +78,9 @@ function themeslug_enqueue_script() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
 	wp_enqueue_style( 'style.screen', trailingslashit( get_template_directory_uri() ) . 'css/style.screen.css', false );
 	wp_enqueue_script( 'jquery');
-	wp_enqueue_script( 'materialize.min', trailingslashit( get_template_directory_uri() ) . 'css/materialize.min.css', false );
-	wp_enqueue_script( 'materialize.min', trailingslashit( get_template_directory_uri() ) . 'js/materialize.min.js', false );
-	wp_enqueue_script( 'font-awesome.min', trailingslashit( get_template_directory_uri() ) . 'css/css/font-awesome.min.css', false );
+	wp_enqueue_script( 'materialize', trailingslashit( get_template_directory_uri() ) . 'css/materialize.min.css', false );
+	wp_enqueue_script( 'materialize', trailingslashit( get_template_directory_uri() ) . 'js/materialize.min.js', false );
+	wp_enqueue_script( 'font-awesome', trailingslashit( get_template_directory_uri() ) . 'css/css/font-awesome.min.css', false );
 }
 add_action( 'wp_enqueue_scripts', 'themeslug_enqueue_script' );
 
