09f84a3f872cc955cdcf5a4dc0aa157d9ee0065c
Michael Boumann <mboumann@outlook.com>
2016-06-10 15:02:56 +0200

    dont load screen css as its gone

--- functions.php
+++ functions.php
@@ -10,7 +10,6 @@ if ( ! isset( $content_width ) ) {
 
 function insideuwp_theme_name_scripts() {
 	wp_enqueue_style( 'core',  get_stylesheet_directory_uri(). '/style.css', false );
-	wp_enqueue_style( 'screen', get_stylesheet_directory_uri() . '/css/style.screen.css', false );
 	wp_enqueue_style( 'materialize', 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css', false );
 	wp_enqueue_style( 'font-awesome', get_stylesheet_directory_uri() . '/css/font-awesome.min.css', false );
 	wp_enqueue_script( 'jquery' );
