3935fd4edd8e48377edd34993dd21326fa96ba4d
Michael Boumann <mboumann@outlook.com>
2016-06-10 16:38:48 +0200

    remove 2 seperate js files

--- js/sidenav.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function(){
-  // Initialize collapse button
-  jQuery('.button-collapse').sideNav({
-        edge: 'right', // Choose the horizontal origin
-        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      }
-    );
- });
