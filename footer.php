80725e4680113cb17430bdf32c9426efa82f6e9f
Michael Boumann <mboumann@outlook.com>
2016-06-12 00:10:59 +0200

    add twitter url

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
 
     <div class="socialfooter">
         <a href="/rss" target="_blank"><i class="fa fa-rss fa-2x"></i></a>
+        <a href="https://twitter.com/insideuwp" target="_blank"><i class="fa fa-twitter fa-2x"></i></a>
         <p class="right">
           <a href="<?php echo home_url();?>/privacy-policy">privacy policy</a>
           <?php
