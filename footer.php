e1bfa54b1bb5c00e5421f0af2e76dd6386cbc2ec
Michael Boumann <mboumann@outlook.com>
2016-06-11 16:23:42 +0200

    add dynamic theme version

--- footer.php
+++ footer.php
@@ -3,7 +3,10 @@
         <a href="/rss" target="_blank"><i class="fa fa-rss fa-2x"></i></a>
         <p class="right">
           <a href="<?php echo home_url();?>/privacy-policy">privacy policy</a>
-          <a href="https://jadewp.demo.insideuwp.xyz" target="_blank">powered by: Jade for Wordpress</a>
+          <?php
+              $my_theme = wp_get_theme();
+              echo $my_theme->get( 'Name' ) . " is version " . $my_theme->get( 'Version' );
+            ?>
         </p>
       </div>
     <!-- close with Wordpress footer aka adminbar etc. -->
