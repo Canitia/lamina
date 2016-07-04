ec9d47a96d56394ec6f4f52502d4fed480f05278
Michael Boumann <mboumann@outlook.com>
2016-07-05 01:42:25 +0200

    update footer with privacy policy and edit param name

--- footer.php
+++ footer.php
@@ -7,8 +7,8 @@
         <p class="right">
           <a href="<?php echo home_url();?>/privacy-policy">privacy policy</a>
           <?php
-              $my_theme = wp_get_theme();
-              echo $my_theme->get( 'Name' ) . " v" . $my_theme->get( 'Version' );
+              $getActiveTheme = wp_get_theme();
+              echo $getActiveTheme->get( 'Name' ) . " v" . $getActiveTheme->get( 'Version' );
             ?>
         </p>
       </div>
