37b7d71d5677697ce5ca11442920613760cfaa7f
Michael Boumann <mboumann@outlook.com>
2016-08-23 23:16:00 +0200

    add single-apps page

---
+++ single-apps.php
@@ -0,0 +1,89 @@
+<?php get_header(); ?>
+
+     <div class="row">
+     <div class="col s12 m12 l8 main-content">
+
+
+
+       <?php
+
+$the_query = new WP_Query(
+	array(
+		'post_type' => array( 'App-updates' ),
+		'posts_per_page' => 5
+	)
+);
+?>
+
+<?php if ( $the_query->have_posts() ) : ?>
+
+	<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+          <div class="card">
+                <div <?php post_class(); ?>>
+                    <article>
+                      <span class="card-title-single">
+                        <p class="posttitle-single"><?php the_title(); ?></p>
+                        </span>
+                      <div class="card-image card-image-single">
+                        <?php if ( has_post_thumbnail() ) {
+                                        the_post_thumbnail( 'large', array( 'class' => 'responsive-img' ) );
+                              } else { ?>
+                                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/no-pic-available.jpg" alt="<?php the_title_attribute(); ?>" class="responsive-img" />
+                                <?php }; ?>
+                      </div>
+                      <div class="card-content">
+                        <p class="postdate center-align">
+                          <i class="fa fa-clock-o"></i><time><?php echo get_the_date(); ?></time>
+                          <i class="fa fa-user-secret"></i>  <?php the_author_posts_link();?>
+                          <?php if( is_sticky() ) {
+                            ?><i class="fa fa-star"></i> Featured
+                        <?php  } ?>
+                        </p>
+                        <p><?php the_content(); ?></p>
+                      </div>
+                      <?php if(has_tag()) { ?>
+                      <div class="tags">
+                        <p><strong>Tags: </strong><?php the_tags( '', ', ', '' ); ?>
+                      </div>
+                      <?php } ?>
+
+                      <section class="author-profile">
+                        <p class="author-bio">
+                      <?php echo get_avatar( get_the_author_meta('email'), '100' ); ?>
+                        <?php the_author_posts_link();?>
+                          <?php edit_post_link('edit', '<i class="fa fa-pencil-square-o" aria-hidden="true"></i>' ); ?>
+                        <br />
+                          <?php echo nl2br(get_the_author_meta('description'));  ?>
+                      </p>
+                      </section>
+                    </article><!-- close article -->
+
+                          <!-- let user enter a comment -->
+                    <?php //comments_template(); ?>
+            </div><!-- close post class div -->
+          </div>
+                <!-- column end! -->
+
+
+
+            <!-- error handling -->
+            <?php endwhile; else: ?>
+                  <p><?php echo wpautop( 'Sorry, this post can not be found' ); ?></p>
+            <?php endif;
+
+            	 wp_reset_postdata(); ?>
+
+            <?php wp_link_pages('before=<ul class="pagination accentcolor2 center-align" role="pagination">&after=</ul>&link_before=<li>&link_after=</li>'); ?>
+          </div><!-- einde md8 -->
+
+<div class="col l4 hide-on-med-and-down">
+<?php get_sidebar( 'primary' ); ?>
+</div>
+
+</div><!-- end row -->
+
+
+</div><!-- container fluid END! -->
+
+<!-- start of footer -->
+<?php get_footer(); ?>
