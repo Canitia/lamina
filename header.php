73d7f848957a7659ed7bc08741c7a3769be151d0
Michael Boumann <mboumann@outlook.com>
2016-08-23 15:50:30 +0200

    back to ul

--- header.php
+++ header.php
@@ -40,14 +40,14 @@
             <a href="<?php echo home_url();  ?>" class="headerurl" style="color: #<?php echo get_header_textcolor(); ?> !important;"><?php bloginfo('name'); ?></a>
         </p>
 
-         <p id="nav-mobile" class="right hide-on-med-and-down">
+         <ul id="nav-mobile" class="right hide-on-med-and-down">
            <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
-         </p>
+         </ul>
          <a href="#" data-activates="slide-out" class="button-collapse right"><i class="fa fa-bars" aria-hidden="true"></i></a>
 
-        <p id="slide-out" class="side-nav">
-            <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
-        </p>
+<ul id="slide-out" class="side-nav">
+    <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+</ul>
 
 </div>
 </nav>
