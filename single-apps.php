9f92b5fc8154019fe42ffb36a745eb0df3108401
Michael Boumann <mboumann@outlook.com>
2016-08-24 11:11:29 +0200

    update qeury

--- single-apps.php
+++ single-apps.php
@@ -27,7 +27,7 @@
                                         </p>
                                         <hr />
                                         <?php
-                                            $args = array( 'post_type' => 'app-updates', 'posts_per_page' => 5, 'parent' => getField('name') );
+                                            $args = array( 'post_type' => 'app-updates', 'posts_per_page' => 5, 'meta_key' => 'parent', 'meta_value' => getField('name') );
                                             ?>
 
                                             <h2>Latest app updates</h2>
