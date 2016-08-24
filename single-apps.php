d70e372acf8c1101fd6608a4eef120b1cae16900
Michael Boumann <mboumann@outlook.com>
2016-08-24 12:34:50 +0200

    name

--- single-apps.php
+++ single-apps.php
@@ -24,7 +24,7 @@
 
                                       //  $parentname = get_field('appparent', get_the_id());
                                       // get only first 3 results
-                                      $ids = get_field('poparent');
+                                      $ids = get_field('name');
                                       var_dump($ids);
 
                                             $args = array( 'post_type' => 'app-updates',
