66c935ee90d2b10c0c028080e19bb2468bb9f25f
Michael Boumann <mboumann@outlook.com>
2016-10-02 01:27:03 +0200

    undo materialize

--- functions.php
+++ functions.php
@@ -11,20 +11,20 @@ if ( ! isset( $content_width ) ) {
  */
 
 function insideuwp_theme_name_scripts() {
-	wp_enqueue_style( 'materialize', 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css', false );
+	wp_enqueue_style( 'materialize', 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css', false );
 	wp_enqueue_style( 'core',  get_stylesheet_directory_uri(). '/style.css', false );
 	wp_enqueue_style( 'font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css', false );
 
 
 	if( !is_admin()){
 	wp_deregister_script('jquery');
-	wp_register_script('jquery', ('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'), false, '3.1.1', true);
+	wp_register_script('jquery', ('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js'), false, '3.0.0', true);
 	wp_enqueue_script('jquery');
 } else {
 		wp_enqueue_script('jquery');
 }
 
-	wp_enqueue_script( 'materialize', 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js', array('jquery'), false, true );
+	wp_enqueue_script( 'materialize', 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js', array('jquery'), false, true );
 	wp_enqueue_script( 'sidenav', get_stylesheet_directory_uri() . '/js/theme.js', array('jquery', 'materialize'), false, true );
 }
 add_action( 'wp_enqueue_scripts', 'insideuwp_theme_name_scripts' );
