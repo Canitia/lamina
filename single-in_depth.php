600879c26a80127bb743a8d25ce7fedc93faf92d
Michael Boumann <mboumann@outlook.com>
2016-10-03 00:11:26 +0200

    center align tags

--- single-in_depth.php
+++ single-in_depth.php
@@ -47,7 +47,7 @@
                                         <p><?php the_content(); ?></p>
                                       </div>
                                       <?php if(has_tag()) { ?>
-                                      <div class="tags">
+                                      <div class="tags center">
                                         <p><strong>Tags: </strong><?php the_tags( '', ', ', '' ); ?>
                                       </div>
                                       <?php } ?>
