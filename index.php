e30cba8696307ae7222d21f383ee3c86cacc0361
Michael Boumann <mboumann@outlook.com>
2016-10-03 01:11:28 +0200

    readd user as things look empty without it

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
                                           <div class="card-content">
                                             <p class="postdate center-align">
                                               <i class="fa fa-clock-o"></i><time><?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . ' ago'; ?></time>
+                                              <i class="fa fa-user-secret"></i>  <?php the_author_posts_link();?>
                                               <?php if( is_sticky() ) {
                                                 ?><i class="fa fa-star"></i> Featured
                                             <?php  } ?>
