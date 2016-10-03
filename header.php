8b40d2b1f9d38f700d5bbd407a2ee33be98f8e8d
Michael Boumann <mboumann@outlook.com>
2016-10-03 16:28:56 +0200

    rm some spaces

--- header.php
+++ header.php
@@ -38,9 +38,9 @@
          </ul>
          <a href="#" data-activates="slide-out" class="button-collapse right"><i class="fa fa-bars" aria-hidden="true"></i></a>
 
-<ul id="slide-out" class="side-nav">
-    <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
-</ul>
+        <ul id="slide-out" class="side-nav">
+            <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+        </ul>
 
 </div>
 </nav>
