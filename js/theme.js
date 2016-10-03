3cfe77132a6d979fc3d95f04bb828a4b7e0e5c79
Michael Boumann <mboumann@outlook.com>
2016-10-03 19:09:28 +0200

    move slideout to the left as well

--- js/theme.js
+++ js/theme.js
@@ -2,7 +2,7 @@
 jQuery(document).ready(function(){
   // Initialize collapse button
   jQuery('.button-collapse').sideNav({
-        edge: 'right', // Choose the horizontal origin
+        edge: 'left', // Choose the horizontal origin
         closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
       }
     );
