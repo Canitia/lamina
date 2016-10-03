9ed66a60729ba5454779198bff5d866444cf9700
Michael Boumann <mboumann@outlook.com>
2016-10-03 17:04:54 +0200

    move action below stuff

--- functions.php
+++ functions.php
@@ -154,8 +154,6 @@ function cptui_register_my_cpts_in_depth() {
 }
 
 
-// Show posts of 'post', 'page' and 'movie' post types on home page
-add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
 
 function add_my_post_types_to_query( $query ) {
   if ( is_home() && $query->is_main_query() ) {
@@ -173,6 +171,10 @@ else
 return $query;
 	}
 }
+
+// Show posts of 'post', 'page' and 'movie' post types on home page
+add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
+
 
 
 ?>
