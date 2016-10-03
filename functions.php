3565209452311bc8032b2ea72be3a6c31ea35261
Michael Boumann <mboumann@outlook.com>
2016-10-03 17:09:34 +0200

    update

--- functions.php
+++ functions.php
@@ -171,7 +171,7 @@ else
 return $query;
 	}
 
-	if ( $query->get( 'post_type' ) === 'nav_menu_item' ) {
+	if ( $query->get( 'post_type' ) === 'menu-item' ) {
 	$query->set( 'tax_query', '' );
 	$query->set( 'tax_query', '' );
 	$query->set( 'meta_key', '' );
