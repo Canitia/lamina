ef88c191cfa0600e8df6710469ae0a17dd5438b6
Michael Boumann <mboumann@outlook.com>
2017-05-15 20:22:20 +0200

    move mobile menu left and cleanup analytics

--- js/theme.js
+++ js/theme.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function(){
   // Initialize collapse button
   jQuery('.button-collapse').sideNav({
     menuWidth: 300, // Default is 300
-    edge: 'right', // Choose the horizontal origin
+    edge: 'left', // Choose the horizontal origin
     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
     draggable: true // Choose whether you can drag to open on touch screens
       }
@@ -13,12 +13,3 @@ jQuery(document).ready(function(){
         indicators: false
         })
  });
-
-//code for analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-63270110-1', 'auto');
-ga('send', 'pageview');
