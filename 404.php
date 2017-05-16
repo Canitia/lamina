9cba5940847963841a823c3b9bc8b5c4d2f2ea80
Michael Boumann <mboumann@outlook.com>
2017-05-16 20:17:00 +0200

    update error translation

--- 404.php
+++ 404.php
@@ -6,10 +6,10 @@
                       <div class="card large card-error">
                           <div class="card-image center">
                             <i class="fa fa-ban fa-5x" aria-hidden="true"></i>
-                            <span class="card-title"><p class="posttitle"><?php __('Error', 'cerulean-for-wordpress'); ?></p></span>
+                            <span class="card-title"><p class="posttitle"><?php _e('Error', 'cerulean-for-wordpress'); ?></p></span>
                           </div>
                           <div class="card-content">
-                            <p><?php __('This page doesnt exist anymore or was available on the old website. To continue please select a menu option from the navigation menu.', 'cerulean-for-wordpress');?></p>
+                            <p><?php _e('This page doesnt exist anymore or was available on the old website. To continue please select a menu option from the navigation menu.', 'cerulean-for-wordpress');?></p>
                           </div>
                       </div>
 
