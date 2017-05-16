6d7aa0693c3b6d8ff534b78af1164ec848602c8e
Michael Boumann <mboumann@outlook.com>
2017-05-16 11:50:40 +0200

    make room for sidebar

--- category.php
+++ category.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
      <div class="row">
-               <div class="col s12 m12 l12 main-content">
+               <div class="col s12 m8 l8 main-content">
                  <h1 class="text-left-title-featured-sidebar">Latest posts in <?php single_cat_title(); ?></h1>
                 <ul class="collection">
                               <?php if ( have_posts() ) : ?>
