d5e56bd85ae628823f9cf8c09514c9effaed404b
Michael Boumann <mboumann@outlook.com>
2017-05-16 23:29:08 +0200

    refer to cerulean-for-wordpress not ghost

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
 <div class="col s12 m4 l4 hide-on-small-only sidebar"><!-- start of a sidebar item -->
 		<ul>
 			<?php if ( ! dynamic_sidebar('primary') ) : ?>
-				<li><?php _e('There are currently no widgets enabled.', 'cerulean-for-ghost'); ?></li>
+				<li><?php _e('There are currently no widgets enabled.', 'cerulean-for-wordpress'); ?></li>
 			<?php endif; ?>
 		</ul>
 </div><!-- end of sidebar item -->
