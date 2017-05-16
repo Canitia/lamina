7fa6e92bd2ed0df7182358f52fcdf02f7308de3f
Michael Boumann <mboumann@outlook.com>
2017-05-16 23:36:43 +0200

    update style.css tags

--- searchform.php
+++ searchform.php
@@ -3,11 +3,11 @@
   <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
       <label>
           <input type="search" class="search-field"
-              placeholder="<?php echo esc_attr_x( 'Search ...', 'placeholder' ) ?>"
+              placeholder="<?php echo esc_attr_x( 'Search ...', 'placeholder', 'cerulean-for-wordpress' ) ?>"
               value="<?php echo get_search_query() ?>" name="s"
-              title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+              title="<?php echo esc_attr_x( 'Search for:', 'label', 'cerulean-for-wordpress' ) ?>" />
       </label>
       <input type="submit" class="search-submit btn grey"
-          value="<?php echo esc_attr_x( 'Go', 'submit button' ) ?>" />
+          value="<?php echo esc_attr_x( 'Go', 'submit button', 'cerulean-for-wordpress' ) ?>" />
   </form>
 </div>
