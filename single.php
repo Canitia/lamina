0c274f4bd4c2ad7e4f90d1c316664d6c11c89b94
Michael Boumann <mboumann@outlook.com>
2017-05-16 10:48:03 +0200

    disable comments as they look so out of place

--- single.php
+++ single.php
@@ -29,7 +29,7 @@
    </article><!-- close article -->
                                           <!-- let user enter a comment -->
                   <h1 class="text-left-title-featured-sidebar">Join the conversation</h1>
-                                		<?php comments_template(); ?>
+                                		<?php //comments_template(); ?>
                             </div><!-- close post class div -->
                           </div>
 
