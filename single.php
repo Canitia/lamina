49354eb92ccbf718148406bdcf6765e313dc2ea9
Michael Boumann <mboumann@outlook.com>
2017-05-24 00:41:50 +0200

    add very light border to main-content

--- single.php
+++ single.php
@@ -4,7 +4,7 @@
                     <?php if ( have_posts() ) : ?>
                         <?php while ( have_posts() ) : the_post(); ?>
  <div>   
-    <div class="col s12 m8 l8">
+    <div class="col s12 m8 l8 main-content">
     <div <?php post_class(); ?>>
         <article>
           <span class="card-title">
