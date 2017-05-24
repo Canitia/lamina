45a3f0c6176ad2c3f89b4779b8b23f593ece6448
Michael Boumann <mboumann@outlook.com>
2017-05-24 20:03:50 +0200

    add partial support for pagination

--- category.php
+++ category.php
@@ -28,20 +28,7 @@
               </ul>
                                       <!-- navigation?-->
 
-                                    <ul class="pagination center-align" role="pagination">
-                                      <?php if( get_previous_posts_link() ) :
-
-                                      previous_posts_link( '<li class="pagination-arrows newer-posts"><i class="fa fa-ellipsis-h"></i></li>' );
-
-                                      endif; ?>
-
-                                      <li class="active"><?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; echo $paged; ?></li>
-                                      <?php if( get_next_posts_link() ) :
-
-                                      next_posts_link( '<li class="pagination-arrows older-posts"><i class="fa fa-ellipsis-h"></i></li>' );
-
-                                      endif; ?>
-                                    </ul>
+                <?php get_template_part( 'partials/pagination' ); ?>
                                     </div><!-- einde md8 -->
 
                   <?php get_sidebar( 'primary' ); ?>
