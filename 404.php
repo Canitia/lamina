ccaa3b10d1036c9bc586f58cc8a426f97ba2fcc2
Michael Boumann <mboumann@outlook.com>
2017-05-25 16:50:01 +0200

    update 404 page with searchform and edit error text

--- 404.php
+++ 404.php
@@ -5,9 +5,9 @@
            <div class="main-content col s12 m8 l8">
                 <h1 class="text-left-title-featured-sidebar"><?php _e('Error', 'cerulean-for-wordpress'); ?></h1>
               <div class="post-content">
-                <p><?php _e('This post or page doesnt exist anymore. To continue please select a menu option from the navigation menu or navigate to the previous page.', 'cerulean-for-wordpress');?></p>
+                <p><?php _e('This post/page doesnt exist anymore or has been renamed. Try searching for it.', 'cerulean-for-wordpress');?></p>
               </div>
-
+<?php get_search_form(); ?>
 </div><!-- container fluid END! -->
     <!-- second column (widget bar) -->
     <?php get_sidebar( 'primary' ); ?>
