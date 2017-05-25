8b4320c423ee281873c0927328bc950a5825cbce
Michael Boumann <mboumann@outlook.com>
2017-05-25 16:58:25 +0200

    set error-text class

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
      <div class="row">
            <div class="main-content col s12 m8 l8">
                 <h1 class="text-left-title-featured-sidebar error"><?php _e('Error', 'cerulean-for-wordpress'); ?></h1>
-              <div class="post-content error">
+              <div class="post-content error-text">
                 <p><?php _e('This post/page doesnt exist anymore or has been renamed. Try searching for it.', 'cerulean-for-wordpress');?></p>
               </div>
 <?php get_search_form(); ?>
