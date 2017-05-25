80e5e04a5f572c3277d25ddb4417478b71e2004a
Michael Boumann <mboumann@outlook.com>
2017-05-26 01:28:45 +0200

    add 'today' section to index page

--- index.php
+++ index.php
@@ -2,15 +2,67 @@
 <div class="container-fluid">
   <div class="row">
     <div class="main-content col s12 m8 l8">
- 
-     <h1 class="text-left-title-featured-sidebar"><?php _e('Latest', 'cerulean-for-wordpress'); ?></h1>
- <ul class="collection">
-    <?php 
-    $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
-    $args = array( 'orderby'=> 'date', 'order' => 'DESC', 'ignore_sticky_posts' => 1, 'paged' => $paged ); 
-    $my_query = new WP_Query( $args );
-    if ( $my_query->have_posts() ) : 
-      while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
+    <?php $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
+      if ($paged < 2) {    
+    ?>
+    <h1 class="text-left-title-featured-sidebar"><?php _e('Today', 'cerulean-for-wordpress'); ?></h1>
+   <ul class="collection">
+     <?php
+
+      $lastweek_args = array(
+        'post_type'      => 'post',
+        'post_status'    => 'publish',
+        'orderby'        => 'date',
+        'order'          => 'DESC',
+        'ignore_sticky_posts' => 1,
+        'date_query'     => array(
+                                    array(
+                                      'column' => 'post_date_gmt',              
+                                      'after' => '1 day ago'
+
+                                    )
+                              )
+      );
+      $lastweek_query = new WP_Query( $lastweek_args );
+
+      if ( $lastweek_query->have_posts() ) {
+        
+        while ( $lastweek_query->have_posts() ) {
+          $lastweek_query->the_post();
+          ?>
+        <a href="<?php the_permalink(); ?>">
+        <li class="collection-item">
+            <p title="<?php the_title_attribute(); ?>" class="truncate"><?php if ( is_sticky() ) {?><i class="fa fa-star <?php echo 'sticky';?>" aria-hidden="true"></i><?php } else {?><i class="fa fa-circle" aria-hidden="true"></i><?php }; the_title(); ?>
+              <span class="badge">
+              <time><?php echo human_time_diff( get_the_time('U'), current_time('timestamp')); echo '&nbsp;'; _e('ago', 'cerulean-for-wordpress'); ?></time>
+            </span> 
+            </p>
+        </li>
+        </a>
+          <?php
+        }
+        
+      }
+      wp_reset_postdata(); 
+      };
+      ?>
+      </ul>
+
+    <h1 class="text-left-title-featured-sidebar"><?php _e('Older posts', 'cerulean-for-wordpress'); if($paged > 1){ echo '<div class="right">' . $paged . '</div>'; }; ?></h1>
+    <ul class="collection">
+    <?php
+    $args = array( 'orderby'=> 'date', 'order' => 'DESC', 'ignore_sticky_posts' => 1, 'paged' => $paged,
+            'date_query'     => array(
+                                    array(
+                                      'column' => 'post_date_gmt',              
+                                      'before' => '1 day ago'
+
+                                    )
+                              )    
+    ); 
+    $main_query = new WP_Query( $args );
+    if ( $main_query->have_posts() ) : 
+      while ( $main_query->have_posts() ) : $main_query->the_post(); ?>
         <a href="<?php the_permalink(); ?>">
         <li class="collection-item">
             <p title="<?php the_title_attribute(); ?>" class="truncate"><?php if ( is_sticky() ) {?><i class="fa fa-star <?php echo 'sticky';?>" aria-hidden="true"></i><?php } else {?><i class="fa fa-circle" aria-hidden="true"></i><?php }; the_title(); ?>
@@ -20,20 +72,20 @@
             </p>
         </li>
         </a><?php endwhile; else: ?>
-                            <!-- error handling -->
-                        		      <p><?php _e('Sorry, it seems there are no posts available.', 'cerulean-for-wordpress'); ?></p>
-                            <?php endif; ?>
-                        </ul>
+        <!-- error handling -->
+        <p><?php _e('Sorry, it seems there are no posts available.', 'cerulean-for-wordpress'); ?></p>
+    <?php endif; ?>
+    </ul>
 
-                            <!-- navigation?-->
+<!-- navigation?-->
 
-                <?php get_template_part( 'partials/pagination' ); ?>
-                    </div><!-- einde md8 -->  <!-- column end! -->
+<?php get_template_part( 'partials/pagination' ); ?>
+  </div><!-- einde md8 -->  <!-- column end! -->
 
-                    <!-- second column (widget bar) -->
-                        <?php get_sidebar( 'primary' ); ?>
+  <!-- second column (widget bar) -->
+  <?php get_sidebar( 'primary' ); ?>
 
-    </div><!-- end container inside -->
+  </div><!-- end container inside -->
 </div><!-- container fluid END! -->
 
 <!-- start of footer -->
--- partials/pagination.php
+++ partials/pagination.php
@@ -1,11 +1,11 @@
 <ul class="pagination center-align" role="pagination">
     <?php if( get_previous_posts_link() ) :
-        previous_posts_link( '<li class="pagination-arrows newer-posts"><i class="fa fa-ellipsis-h"></i></li>' );
+        previous_posts_link( '<li class="pagination-arrows newer-posts"><i class="fa fa-angle-left" aria-hidden="true"></i></li>' );
     endif; ?>
 
     <li class="active"><?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; echo $paged; ?></li>
     
     <?php if( get_next_posts_link() ) :
-        next_posts_link( '<li class="pagination-arrows older-posts"><i class="fa fa-ellipsis-h"></i></li>' );
+        next_posts_link( '<li class="pagination-arrows older-posts"><i class="fa fa-angle-right" aria-hidden="true"></i></li>' );
     endif; ?>
 </ul>
