679232940d140bff14eb5fa7e09bf1190678cbe7
Michael Boumann <mboumann@outlook.com>
2017-05-25 16:54:40 +0200

    add class for red error text header

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
    <div class="container-fluid">
      <div class="row">
            <div class="main-content col s12 m8 l8">
-                <h1 class="text-left-title-featured-sidebar"><?php _e('Error', 'cerulean-for-wordpress'); ?></h1>
+                <h1 class="text-left-title-featured-sidebar error"><?php _e('Error', 'cerulean-for-wordpress'); ?></h1>
               <div class="post-content">
                 <p><?php _e('This post/page doesnt exist anymore or has been renamed. Try searching for it.', 'cerulean-for-wordpress');?></p>
               </div>
