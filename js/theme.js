11535207a5b7786ad714e98169fc2c5cf7d631db
Michael Boumann <mboumann@outlook.com>
2017-05-28 02:36:19 +0200

    remove old materialize js

--- js/theme.js
+++ js/theme.js
@@ -1,19 +1,4 @@
 // code to enable side navigation
 jQuery(document).ready(function () {
-  // Initialize collapse button
-  jQuery('.button-collapse').sideNav({
-    menuWidth: 300, // Default is 300
-    edge: 'left', // Choose the horizontal origin
-    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    draggable: true // Choose whether you can drag to open on touch screens
-  });
-
   jQuery('.carousel').carousel();
-
-  jQuery('select').material_select();
-
-    // Configure dropdown
-    jQuery(".dropdown-button").dropdown({
-        hover: false
-    });
 });
