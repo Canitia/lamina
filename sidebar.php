57006989d0d12b3650d9613807fb484166ac1f71
Michael Boumann <mboumann@outlook.com>
2017-05-28 19:14:18 +0200

    hide sidebar on sm and down

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-<div class="col-md-4 col-lg-4 hidden-xs-down sidebar"><!-- start of a sidebar item -->
+<div class="col-md-4 col-lg-4 hidden-sm-down sidebar"><!-- start of a sidebar item -->
 			<?php if ( ! dynamic_sidebar('primary') ) : ?>
 				<li><?php _e('There are currently no widgets enabled.', 'cerulean-for-wordpress'); ?></li>
 			<?php endif; ?>
