7a692f00fdc11d1019c9363461d50fbd2d23a77e
Michael Boumann <mboumann@outlook.com>
2017-05-28 19:21:25 +0200

    move searchform within div

--- single.php
+++ single.php
@@ -57,8 +57,8 @@
       <?php endwhile; else: ?>
           <div class="post-content">
             <p><?php _e('Sorry, this post can not be found or has been deleted.', 'cerulean-for-wordpress'); ?></p>
+            <?php get_search_form(); ?>
         </div><!-- post-content END! -->
-         <?php get_search_form(); ?>
       <?php endif; ?>
 
 
