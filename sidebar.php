c69f833e01dced6ac4ecd318df1968e275d62ed5
Michael Boumann <mboumann@outlook.com>
2017-05-29 11:58:44 +0200

    alignment tweaks to sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 <div class="col-md-4 col-lg-4 hidden-sm-down sidebar"><!-- start of a sidebar item -->
-			<?php if ( ! dynamic_sidebar('primary') ) : ?>
-				<li><?php _e('There are currently no widgets enabled.', 'cerulean-for-wordpress'); ?></li>
-			<?php endif; ?>
+	<?php if ( ! dynamic_sidebar('primary') ) : ?>
+		<li><?php _e('There are currently no widgets enabled.', 'cerulean-for-wordpress'); ?></li>
+	<?php endif; ?>
 </div><!-- end of sidebar item -->
