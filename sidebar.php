a90ea7402e3d0b4558aabb2798adf1959d43ccb0
Michael Boumann <mboumann@outlook.com>
2017-05-30 13:14:36 +0200

    use div around sidebar error message

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 <div class="col-md-4 col-lg-4 hidden-sm-down sidebar"><!-- start of a sidebar item -->
 	<?php if ( ! dynamic_sidebar('primary') ) : ?>
-		<li><?php _e('There are currently no widgets enabled.', 'canitia'); ?></li>
+		<div><?php _e('There are currently no widgets enabled.', 'canitia'); ?></div>
 	<?php endif; ?>
 </div><!-- end of sidebar item -->
