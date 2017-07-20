758d3177c9d14b5c7baf2400cb416a7e30004e60
Michael Boumann <michaelboumann@gmail.com>
2017-07-21 00:35:55 +0200

    move author name ouf of author bio div

--- partials/authorsection.php
+++ partials/authorsection.php
@@ -2,6 +2,7 @@
 
 <section class="author-profile">
 <hr />
+    <strong class="author-name"><?php the_author_posts_link();?></strong>
 <?php 
     if ( get_the_author_meta( 'description' ) ) {
     echo get_avatar( get_the_author_meta('email'), '100' ); 
@@ -11,8 +12,7 @@
     }
 
 ?>
-    <p class="author-bio">
-    <strong class="author-name"><?php the_author_posts_link();?></strong>
+<p class="author-bio">
 <br />
 <?php echo nl2br(get_the_author_meta('description'));  ?>
 </p>
