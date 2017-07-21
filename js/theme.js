a99434ac0c827a3dfc75cb0ab325329e3062b9dc
Michael Boumann <michaelboumann@gmail.com>
2017-07-21 10:55:31 +0200

    revert to click to open drowndowns #84

--- js/theme.js
+++ js/theme.js
@@ -1,16 +1,5 @@
 // code to enable side navigation
 jQuery(document).ready(function () {
   jQuery('.carousel').carousel();
-
-  jQuery(function($) {
-  $('.navbar .dropdown').hover(function() {
-  $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
-
-  }, function() {
-  $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
-
-  });
-
-})
 }
 );
