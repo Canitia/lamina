109761e1a5950c525909ea10e723d29dd9eba3d1
Michael Boumann <michaelboumann@gmail.com>
2017-07-23 01:09:42 +0200

    only show bio when description is there

--- partials/authorsection.php
+++ partials/authorsection.php
@@ -1,24 +1,25 @@
 
 
 <section class="author-profile">
-<hr />
+<hr class="cat-links-hr" />
     <strong class="author-name"><?php the_author_posts_link();?></strong>
 <?php 
     if ( get_the_author_meta( 'description' ) ) {
     echo get_avatar( get_the_author_meta('email'), '100' ); 
-    } /* end author meta */
+ ?>
+        <p class="author-bio">
+        <br />
+        <?php echo nl2br(get_the_author_meta('description'));  ?>
+        </p>
+ <?php  } /* end author meta */
     else {
       echo get_avatar( get_the_author_meta('email'), '50' );       
     }
 
 ?>
-<p class="author-bio">
-<br />
-<?php echo nl2br(get_the_author_meta('description'));  ?>
-</p>
 </section>
 <?php if ( !is_page() ) { ?>
-<hr />
+<hr class="cat-links-hr" />
 <?php } /* end is page */
 
 
