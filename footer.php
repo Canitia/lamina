16c95120c9b321b2984b04bcd51f39564ce3e789
Michael Boumann <michaelboumann@gmail.com>
2017-07-23 19:20:48 +0200

    use correct display_social

--- footer.php
+++ footer.php
@@ -6,6 +6,11 @@
 
               echo 'Powered by ' . ' <strong>' . $getActiveTheme->get( 'Name' ) . ' </strong>';
             ?>
+        <?php if ( get_theme_mod( 'display_social', 'show' ) == 'show' ) : ?>
+          <div class="author-social float-right">
+              <i class="fa fa-twitter-square" aria-hidden="true"></i>
+          </div>
+      <?php endif; ?>        
         </p>
       </div>
           <!-- close with Wordpress footer aka adminbar etc. -->
@@ -13,4 +18,3 @@
 </footer>
   </body>
 </html>
-
