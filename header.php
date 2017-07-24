e1c0d982c977a53b08828e39a62ef961a50167ae
Michael Boumann <michaelboumann@gmail.com>
2017-07-24 12:12:51 +0200

    [workaround]only show mobile search on pages with a postloop

--- header.php
+++ header.php
@@ -31,6 +31,8 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <?php bootstrap_nav(); ?>
+
+    <?php if ( is_home() || is_category() || is_author() || is_search() ) {?>
     <form class="form-inline col-lg-0 hidden-md-up search-form">
         <input type="search" class="search-field form-control mr-sm-2"
         placeholder="<?php echo esc_attr_x( 'What are you looking for?', 'placeholder', 'canitia' ) ?>"
@@ -38,6 +40,7 @@
         title="<?php echo esc_attr_x( 'Search for:', 'label', 'canitia' ) ?>" maxlength="50" />
         <button class="search-submit btn col-sm-0" type="submit" value="<?php echo esc_attr_x( 'Go', 'submit button', 'canitia' ) ?>">Search</button>
     </form>
+    <?php } ?>
   </div>
 </nav>
 
