8c4f45ab4b524c75f5d3f1ddf97f5ece53f4e6b8
Michael Boumann <michaelboumann@gmail.com>
2017-07-24 22:55:40 +0200

    tweaks for firefox

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 <?php if ( is_active_sidebar( 'primary' ) ) : ?>
-<div class="col-md-4 col-lg-4 hidden-sm-down sidebar d-flex flex-column"><!-- start of a sidebar item -->
+<div class="col-md-4 col-lg-4 hidden-sm-down sidebar"><!-- start of a sidebar item -->
 	<?php dynamic_sidebar( 'primary' ); ?>
 </div><!-- #primary-sidebar -->
 <?php endif; ?>
