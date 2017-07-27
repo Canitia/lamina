c9625a43e4b77cb6f975ea540ba0d6cca949c9bf
Michael Boumann <michaelboumann@gmail.com>
2017-07-27 14:22:23 +0200

    stop using floats for author section

--- partials/authorsection.php
+++ partials/authorsection.php
@@ -1,11 +1,11 @@
 
 
 <section class="author-profile">
-    <strong class="author-name"><?php the_author_posts_link();?></strong>
 <?php 
     if ( get_the_author_meta( 'description' ) ) {
     echo get_avatar( get_the_author_meta('email'), '100' ); 
 ?>
+    <strong class="author-name"><?php the_author_posts_link();?></strong>
         <p class="author-bio">
         <br />
         <?php echo nl2br(get_the_author_meta('description'));  ?>
