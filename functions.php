766ab2a0dd25d5340ba8196d939b4b6b5b36537a
Michael Boumann <michaelboumann@gmail.com>
2017-07-29 00:58:13 +0200

    bump to 3.2.1 jquery

--- functions.php
+++ functions.php
@@ -48,7 +48,7 @@ function canitia_theme_scripts() {
 function include_custom_jquery() {
 
 	wp_deregister_script('jquery');
-	wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', array(), null, true);
+	wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js', array(), null, true);
 
 }
 add_action('wp_enqueue_scripts', 'include_custom_jquery');
