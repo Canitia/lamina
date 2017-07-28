bf99d36720486ff64cf667e33a140162859d67db
Michael Boumann <michaelboumann@gmail.com>
2017-07-28 18:57:58 +0200

    sidebar fix? #105

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,4 @@
-<?php if ( is_active_sidebar( 'primary' ) ) : ?>
+
 <div class="col-md-4 col-lg-4 hidden-sm-down sidebar"><!-- start of a sidebar item -->
 	<?php dynamic_sidebar( 'primary' ); ?>
 </div><!-- #primary-sidebar -->
-<?php endif; ?>
