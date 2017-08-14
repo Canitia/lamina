d4d10b78a390118dc101062b33c98b47355a9752
Michael Boumann <michaelboumann@gmail.com>
2017-08-14 23:04:18 +0200

    only show author section/author image when theres a description

--- partials/authorsection.php
+++ partials/authorsection.php
@@ -1,20 +1,15 @@
-
-
 <section class="author-profile">
-<p class="postedby"><?php _e('This article was written by', 'canitia'); ?></p>
+<?php if ( get_the_author_meta( 'description' ) ) { ?>
+<p class="postedby"><?php _e('This article was written by ', 'canitia');?></p>
 <?php 
-    if ( get_the_author_meta( 'description' ) ) {
     echo get_avatar( get_the_author_meta('email'), '100' ); 
 ?>
-    <strong class="author-name"><?php the_author_posts_link();?></strong>
-        <p class="author-bio">
-        <br />
-        <?php echo nl2br(get_the_author_meta('description'));  ?>
-        </p>
+<strong class="author-name"><?php the_author_posts_link();?></strong>
+    <p class="author-bio">
+    <br />
+    <?php echo nl2br(get_the_author_meta('description'));  ?>
+    </p>
  <?php  } /* end author meta */
-    else {
-      echo get_avatar( get_the_author_meta('email'), '50' );       
-    }
 
 ?>
 </section>
