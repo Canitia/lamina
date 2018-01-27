8fb2d869e8664bcf94f294c0f275f28a90586b08
Michael Boumann <michaelboumann@gmail.com>
2018-01-27 10:52:46 +0100

    make button translatable and add css for it

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
                 ?>
               <h2 class="card-title"><?php the_title(); ?></h2>
               <p class="card-text"><?php the_excerpt(); ?></p>
-              <a href="<?php the_permalink(); ?>" class="btn btn-primary">Go somewhere</a>
+              <a href="<?php the_permalink(); ?>" class="btn btn-primary"><?php _e('Read', 'canitia');?></a>
             </div>
           </div>     
 
