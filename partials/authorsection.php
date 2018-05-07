8344c549c9f0e9b35fb8beb119a3240bf0ac2a75
Michael Boumann <git@michaelboumann.com>
2018-05-07 11:22:35 +0200

    initial comments styling

--- partials/authorsection.php
+++ partials/authorsection.php
@@ -4,7 +4,7 @@
 <?php if ( get_the_author_meta( 'description' ) ) { ?>
 <div class="col-sm-2 col-md-2">
 <?php 
-    echo get_avatar( get_the_author_meta('email'), '100', $default, $alt, array( 'class' => array( 'avatar' ) ) ); 
+    echo get_avatar( get_the_author_meta('email'), '100', $default, $alt, array( 'class' => array( 'avatar-post' ) ) ); 
 ?>
 </div>
 <div class="col-sm-10 author-bio-text">
