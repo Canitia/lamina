d7bfeed54d9a879806288d0036fe73304036ee1a
Michael B <michael@canitia.nl>
2018-05-07 10:15:08 +0200

    add url to title

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
                             </span>
                 <?php
                 } ?>
-              <h2 class="card-title"><?php the_title(); ?></h2>
+              <h2 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
               <p class="card-text"><?php echo  get_the_excerpt(); ?></p>
                 <div class="card-actions-top">
                     <small class="text-muted badge">
