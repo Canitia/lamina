0fc72fef945d408fdb8a1a836dc91814082fa741
Michael Boumann <git@michaelboumann.com>
2019-05-28 19:13:12 +0200

    fix single post head image width

--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <?php
         if ( has_post_thumbnail() ) {
-            the_post_thumbnail('full', ['class' => 'img-fluid', 'title' => 'Feature image']);
+            the_post_thumbnail('full', ['class' => 'post-head-image', 'title' => 'Feature image']);
         } else {
 ?>
 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/no-pic-available.jpg" alt="<?php the_title(); ?>" class="img-fluid" />
