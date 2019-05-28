6261e496d15b1b60a1af4dcb555bf9e3ede3565d
Michael <git-public@canitia.nl>
2019-05-28 16:27:42 +0200

    update header image arguments

--- functions.php
+++ functions.php
@@ -43,8 +43,6 @@ add_theme_support( "title-tag" );
 add_theme_support( 'automatic-feed-links' );
 add_theme_support( "post-thumbnails" );
 $headerargs = array(
-	'flex-width'    => true,
-	'width'         => 980,
 	'flex-height'    => true,
 	'height'        => 200,
 	'default-image' => get_template_directory_uri() . '/images/header.jpg',
