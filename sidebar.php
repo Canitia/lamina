2c102b8d9bd6c1f9b95f3d1ec85ce8db6c15e859
Michael <git-public@canitia.nl>
2019-05-28 16:44:58 +0200

    show more cards on md and lg sizes

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,3 @@
-<div class="col-md-4 col-lg-4 d-none d-md-block sidebar"><!-- start of a sidebar item -->
+<div class="col-md-3 col-lg-3 d-none d-md-block sidebar"><!-- start of a sidebar item -->
 	<?php dynamic_sidebar( 'primary' ); ?>
 </div><!-- #primary-sidebar -->
