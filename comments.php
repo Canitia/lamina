d6a71f1b33e040f01d963e06dc303b6e6b3a4fb3
Michael Boumann <git@michaelboumann.com>
2019-05-29 12:15:32 +0200

    add slight bg to sidebar

--- comments.php
+++ comments.php
@@ -3,7 +3,7 @@ if ( post_password_required() )
 return;
 ?>
 <div class="comments-wrapper">
-
+<h1 class="text-left-title-featured-sidebar text-center home-top-recent"><?php _e('Comments', 'lamina'); ?></h1>
 <?php if ( have_comments() ) : ?>
 	<ul class="commentlist">
 		<?php wp_list_comments();
